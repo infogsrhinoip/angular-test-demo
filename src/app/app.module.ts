da6a689c473cd944ae1ac3fbf1524ccd45125ee6
Sara Chipps <4311361+infogsrhinoip@users.noreply.github.com>
2018-04-19 10:20:54 -0400

    commiting example code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NameComponent } from './name/name.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameComponent
   ],
   imports: [
     BrowserModule
